5eb531504681a9493d75bc88cb1caf45870361bb
Kolupula Raju <rkolupula589@gmail.com>
2026-01-19 17:21:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,52 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "my-vm" {
-  ami           = "ami-02b8269d5e85954ef"   # Amazon Linux 2 (Mumbai)
+resource "aws_instance" "vm" {
+  ami           = "terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "vm" {
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
-  key_name      = "mumbai.key.pem"             # CHANGE THIS
+  key_name      = "mumbai.key.pem"   # MUST exist in AWS
 
   tags = {
     Name = "terraform-ec2"
-    Env  = "dev"
   }
 }
+
+output "instance_public_ip" {
+  value = aws_instance.vm.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.vm.id
+}
+"
+  instance_type = "t2.micro"
+  key_name      = "my-keypair"   # MUST exist in AWS
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
+
+output "instance_public_ip" {
+  value = aws_instance.vm.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.vm.id
+}
+
 
