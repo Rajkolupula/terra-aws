8f692449dc8b425d7e49ecaf9ff2c2f4992f9db4
Kolupula Raju <rkolupula589@gmail.com>
2026-01-19 17:15:26 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,8 @@
 output "instance_public_ip" {
-  value = aws_instance.my_vm.public_ip
+  value = aws_instance.vm.public_ip
 }
 
 output "instance_id" {
-  value = aws_instance.my_vm.id
+  value = aws_instance.vm.id
 }
+
