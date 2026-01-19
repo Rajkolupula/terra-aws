f8259935073e02e535bae0a1b73d6d819df8b9a7
Kolupula Raju <rkolupula589@gmail.com>
2026-01-19 17:26:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,23 +12,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "vm" {
-  ami           = "terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"
-}
-
-resource "aws_instance" "vm" {
-  ami           = "ami-0f5ee92e2d63afc18"
+  ami           = "ami-02b8269d5e85954ef"
   instance_type = "t2.micro"
-  key_name      = "mumbai.key.pem"   # MUST exist in AWS
+  key_name      = "mumbai-key"   # KEY PAIR NAME (not .pem file)
 
   tags = {
     Name = "terraform-ec2"
@@ -42,21 +28,4 @@ output "instance_public_ip" {
 output "instance_id" {
   value = aws_instance.vm.id
 }
-"
-  instance_type = "t2.micro"
-  key_name      = "my-keypair"   # MUST exist in AWS
-
-  tags = {
-    Name = "terraform-ec2"
-  }
-}
-
-output "instance_public_ip" {
-  value = aws_instance.vm.public_ip
-}
-
-output "instance_id" {
-  value = aws_instance.vm.id
-}
-
 
